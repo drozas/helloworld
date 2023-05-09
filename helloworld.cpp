c1f583674c95a0fc342b33e51ecb29e37f455d71
David Rozas <david.rozas@gmail.com>
2023-05-09 15:14:35 +0200

    Update helloworld.cpp

--- helloworld.cpp
+++ helloworld.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 int main()
 {
-    vector<string> msg {"Helloa", "C++", "World", "from", "VS Code", "and the C++ extension!"};
+    vector<string> msg {"Helloaaaa", "C++", "World", "from", "VS Code", "and the C++ extension!"};
     for (const string& word : msg)
     {
         cout << word << " ";
